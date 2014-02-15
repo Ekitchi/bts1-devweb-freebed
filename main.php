4f3045236010b0cf092076cd67a509952a02fbb9
Wal Low <anti.factions@gmail.com>
2014-02-15 12:25:06 +0100

    Modification d'un name qui était faux. Test de Place hold it et par la même occassion, de git.

--- main.php
+++ main.php
@@ -1,56 +1,65 @@
 <!DOCTYPE html>
 <html>
-		<title>FreeBed Location</title>
-		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
-		<link rel="stylesheet" type="text/css" href="style.css"/>
-		<script src="jquery-1.10.2.min.js"></script>
+	<title>FreeBed Location</title>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+	<script src="jquery-1.10.2.min.js"></script>
 
 	<head>
 		<meta charset="UTF-8"/>
 		<title>FreeBed Location</title>
 	</head>
-	
 
 	<body>
 		<header>
 			<nav id="nav" name="nav">
 				<ul id="rechercher" name="rechercher">
-					<li> <input type="text"  value="Où allez-vous ?" style="height:30px; width:250px; border-radius:4px;"> </input> </li>
-					<li> <input type="button" value="Trouver !" style="height:34px; border-radius:4px;"/> </li>
+					<li>
+						<input type="text"  value="Où allez-vous ?" style="height:30px; width:250px; border-radius:4px;">
+						</input>
+					</li>
+					<li>
+						<input type="button" value="Trouver !" style="height:34px; border-radius:4px;"/>
+					</li>
 				</ul>
-				
+
 				<ul id="connexion" name="connexion">
-					<li> <a href=""> Inscription </a> </li>
-					<li> <a href=""> Connexion </a> </li>
+					<li>
+						<a href=""> Inscription </a>
+					</li>
+					<li>
+						<a href=""> Connexion </a>
+					</li>
 				</ul>
 			</nav>
 		</header>
-		
+
 		<section id="firstarticle" name="firstarticle">
 			<article id="slider1" name="slider1">
 				SLIDER1
+				<!-- <img src="http://placehold.it/1515x600"> -->
 			</article>
 		</section>
-		
+
 		<section id="bestrated" name="bestrated">
-			<article id="best" name="renteddesc">
-				IMAGE1
+			<article id="best" name="best">
+				<img src="http://placehold.it/1100x500">
 			</article>
+
 			<article id="bestdesc" name="bestdesc">
 				DESC
 			</article>
 		</section>
-		
+
 		<section id="mostrented" name="mostrented">
 			<article id="renteddesc" name="renteddesc">
 				DESC
 			</article>
+			
 			<article id="rented" name="rented">
-				IMAGE2
+				<img src="http://placehold.it/1100x500">
 			</article>
 		</section>
-		
-		
-		
+
 	</body>
 </html>
