b67f63bdb7c4888ff4201da94999fb740b0644f5
Guilhem "Erw the Evangelist" MAS-PAITRAULT <guilhem.mas-paitrault@infosup.com>
2014-02-19 11:24:19 +0100

    Améliorations multiples de l'interface admin

--- admin/index.php
+++ admin/index.php
@@ -11,50 +11,63 @@
 			<h2>Bienvenue,</h2>
 			<h3>Administrateur</h3>
 			<ul>
-				<li>Tableau de bord</li>
-				<li>Gestion des utilisateurs</li>
-				<li>Gestion des locations</li>
+				<li class="selected">
+					Tableau de bord
+				</li>
+				<li>
+					Gestion des utilisateurs
+				</li>
+				<li>
+					Gestion des locations
+				</li>
 			</ul>
 		</menu>
+		
 		<section>
 			<header>
 				<h1>Administration</h1>
+				<hr />
 			</header>
+			
+			<div id="panel"></div>
 			<article>
 				<header>
 					<h2>Tableau de bord</h2>
 				</header>
-				<section></section>
+				<article></article>
 				<footer></footer>
 			</article>
 			<article>
 				<header>
 					<h2>Gestion des utilisateurs</h2>
 				</header>
-				<section>
-					<tr>
-						<th>ID</th>
-						<th>Nom</th>
-						<th>Prénom</th>
-						<th>Adresse</th>
-						<th>Actions</th>
-					</tr>
-				</section>
+				<article>
+					<table>
+						<tr>
+							<th>ID</th>
+							<th>Nom</th>
+							<th>Prénom</th>
+							<th>Actions</th>
+						</tr>
+					</table>
+				</article>
 				<footer></footer>
 			</article>
 			<article>
 				<header>
 					<h2>Gestion des locations</h2>
 				</header>
-				<section>
-					<tr>
-						<th>ID</th>
-						<th>Nom</th>
-						<th>Adresse</th>
-						<th>Description</th>
-						<th>Actions</th>
-					</tr>
-				</section>
+				<article>
+					<table>
+						<tr>
+							<th>ID</th>
+							<th>Nom</th>
+							<th>Adresse</th>
+							<th>Description</th>
+							<th>Actions</th>
+						</tr>
+					</table>
+				</article>
 				<footer></footer>
 			</article>
 		</section>
