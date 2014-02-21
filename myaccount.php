3971f7ebe61bd3a82898bc4d5961975a82caadb6
Wal Low <anti.factions@gmail.com>
2014-02-22 00:34:14 +0100

    MaJ de myaccount.php Trouvaille d'une parade pour parer (\o/) le décalage des div à l'ouverture d'un onglet

--- myaccount.php
+++ myaccount.php
@@ -11,55 +11,51 @@
 
 	<body>
 		<!-- Inclusion du header -->
-		<?php include_once ("header.php"); ?>
-		
+		<?php
+			include_once ("header.php");
+		?>
 
+		<section>
+			<article>
+				<ul id="menu_account">
+					<li>
+						<a href="#account_profil"> <h1> Profil </h1> </a>
+						<div id="account_profil">
+							<article id="profil_img">
+								IMG
+							</article>
+							<article id="profil_infos">
+								INFORMATIONS PERSONNELS
+							</article>
+						</div>
+					</li>
+					<li>
+						<a href="#account_logements"> <h1> Mes logements </h1> </a>
+						<div id="account_logements">
+							LOGEMENTS MIS EN LOCATION
+						</div>
+					</li>
+					<li>
+						<a href="#account_modifier"> <h1> Modifier mes logements </h1> </a>
+						<div id="account_modifier">
+							MODOFICATION
+						</div>
+					</li>
+					<li>
+						<a href="#account_historique"> <h1> Historique </h1> </a>
+						<div id="account_historique">
+							HISTORIQUES DES LOCATIONS
+						</div>
+					</li>
+					<li>
+						<a href="#account_contact"> <h1> Contact </h1> </a>
+						<div id="account_contact">
+							PRENEZ CONTACT
+						</div>
+					</li>
+				</ul>
+			</article>
+		</section>
 
-		<body>
-			<section>
-				<article>
-					<ul id="menu_account">
-						<li>
-							<a href="#account_profil"> <h1> Profil </h1> </a>
-							<div id="account_profil">
-								<article id="profil_img">
-									IMG
-								</article>
-								<article id="profil_infos">
-									INFORMATIONS PERSONNELS
-								</article>
-							</div>
-						</li>
-						<li>
-							<a href="#account_logements"> <h1> Mes logements </h1> </a>
-							<div id="account_logements">
-								LOGEMENTS MIS EN LOCATION
-							</div>
-						</li>
-						<li>
-							<a href="#account_modifier"> <h1> Modifier mes logements </h1> </a>
-							<div id="account_modifier">
-								MODOFICATION
-							</div>
-						</li>
-						<li>
-							<a href="#account_historique"> <h1> Historique </h1> </a>
-							<div id="account_historique">
-								HISTORIQUES DES LOCATIONS
-							</div>
-						</li>
-						<li>
-							<a href="#account_contact"> <h1> Contact </h1> </a>
-							<div id="account_contact">
-								PRENEZ CONTACT
-							</div>
-						</li>
-					</ul>
-				</article>
-			</section>	
-			
-			
-			
-		</body>
+	</body>
 </html>
-		
