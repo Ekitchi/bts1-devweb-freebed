39a6b5cb87c6a179a0251fbd78aa0c3aa9f2f7d6
Wal Low <anti.factions@gmail.com>
2014-02-22 00:54:23 +0100

    MaJ du header (version pas si finale que ça)

--- header.php
+++ header.php
@@ -42,35 +42,6 @@
 						</tr>
 					</table>
 				</div>
-				
-				
-				
-				<!--<div>
-					<table>
-						<tr>
-							<td><label for="username"> Nom d'utilisateur: </label></td>
-							<td>
-							<input type="text" placeholder="Nom d'utilisateur" id="username" name="username"/>
-							</td>
-						</tr>
-						<tr>
-							<td><label for="password"> Mot de passe: </label></td>
-							<td>
-							<input type="password" placeholder="Mot de passe" id="password" name="password"/>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="2">
-							<input type="checkbox" id="remember" name="remember"/>
-							<label for="remember">Se souvenir de moi</label></td>
-						</tr>
-						<tr>
-							<td colspan="2">
-							<input type="button" value="Se connecter"/>
-							</td>
-						</tr>
-					</table>
-				</div>-->
 			</li>
 		</ul>
 	</nav>
