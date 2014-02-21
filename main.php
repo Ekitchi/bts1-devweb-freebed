f85ca5ce631c8ae33ccbd891972087f980171bf4
Guilhem "Erw the Evangelist" MAS-PAITRAULT <guilhem.mas-paitrault@infosup.com>
2014-02-21 10:50:54 +0100

    Issue 3: Inclure le header   - Ajout du fichier header.php contenant le header   - Modification des fichiers:     - account.php     - inscription.php     - main.php     - recherche.php     - recherche(nominimap).php   afin qu'il utilisent header.php

--- main.php
+++ main.php
@@ -10,54 +10,8 @@
 	</head>
 
 	<body>
-		<header>
-			<nav id="menu" name="menu">
-				<ul id="rechercher" name="rechercher">
-					<li>
-						<input type="text"  placeholder="Où allez-vous ?" style="height:30px; width:250px; border-radius:4px;">
-						</input>
-					</li>
-					<li>
-						<input type="button" value="Trouver !" style="height:34px; border-radius:4px;"/>
-					</li>
-				</ul>
-
-				<ul id="connexion" name="connexion">
-					<li id="register">
-						<a href="inscription.php"> Inscription </a>
-					</li>
-					<li id="login">
-						<a href=""> Connexion </a>
-						<div>
-							<table>
-								<tr>
-									<td><label for="username"> Nom d'utilisateur: </label></td>
-									<td>
-									<input type="text" placeholder="Nom d'utilisateur" id="username" name="username"/>
-									</td>
-								</tr>
-								<tr>
-									<td><label for="password"> Mot de passe: </label></td>
-									<td>
-									<input type="password" placeholder="Mot de passe" id="password" name="password"/>
-									</td>
-								</tr>
-								<tr>
-									<td colspan="2">
-									<input type="checkbox" id="remember" name="remember"/>
-									<label for="remember">Se souvenir de moi</label></td>
-								</tr>
-								<tr>
-									<td colspan="2">
-									<input type="button" value="Se connecter"/>
-									</td>
-								</tr>
-							</table>
-						</div>
-					</li>
-				</ul>
-			</nav>
-		</header>
+		<!-- Inclusion du header -->
+		<?php include_once("header.php"); ?>
 
 		<section id="firstarticle" name="firstarticle">
 			<article id="slider1" name="slider1">
