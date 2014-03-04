9e83642159c84636759e27377982ebdca4bc99f1
Guilhem "Erw the Evangelist" MAS-PAITRAULT <guilhem.mas-paitrault@infosup.com>
2014-03-04 20:53:37 +0100

    Ajout de la page erreur_login.php vers laquelle devront rediriger toutes les erreurs (ou tentatives malhonnêtes) de connexion.

---
+++ erreur_login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Freebed - Accès refusé</title>
+		<meta charset="UTF-8" />
+	</head>
+	<body>
+		<h1>Erreur</h1>
+		<h2>La page à laquelle vous tentez d'accéder ne peut être affichée</h2>
+		<h3>(peut-être n'avez vous pas les droits nécessaires)</h3>
+		<a href="main.php">Retour à l'accueil</a>
+	</body>
+</html>
