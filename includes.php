05995e80a27f429b06a8a04811e95842d7fac52b
Guilhem "Erw the Evangelist" MAS-PAITRAULT <guilhem.mas-paitrault@infosup.com>
2014-03-04 20:52:16 +0100

    Ajout d'un fichier includes.php qui :   - Inclu cl_User.php   - Démarre la session   - Inclu connexion.php   - Inclu login.php

---
+++ includes.php
@@ -0,0 +1,6 @@
+<?php
+	include_once ("cl_User.php");  // La classe User, essentielle pour stocker les infos de l'utilisateur
+	session_start();  // Démarrage de session
+	include_once("connexion.php");  // Gère l'accès à la base de données
+	include_once("login.php");  // Foncions logIn et logOut
+?>
