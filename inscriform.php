c1ffcf94aca14489155e20c49d2aae967a891fc4
William <yoshi93@gmail.com>
2014-03-06 09:48:03 +0100

    Inscriform qui marche PRESQUE.

--- inscriform.php
+++ inscriform.php
@@ -1,26 +1,20 @@
 <?php
-	include("connexion.php");
-	$query = "SELECT * FROM user";
-	$resultat = mysqli_query ($connexion, $query);
+include ("connexion.php");
+$query = "SELECT * FROM user";
+$resultat = mysqli_query($connexion, $query);
 
-	if ( isset($_POST["send"])){
-		$nom = $_POST["nom"];
-		$prenom = $_POST["prenom"];
-		$email = $_POST["email"];
-		$password = $_POST["password_insc"];
-		$oqp = $_POST["occupation"];
-		$tel = $_POST["telephone"];
+if (isset($_POST["email"]) && isset($_POST["nom"]) && isset($_POST["prenom"])) {
+	$nom = $_POST["nom"];
+	$prenom = $_POST["prenom"];
+	$email = $_POST["email"];
+	$password = $_POST["password_insc"];
+	$oqp = $_POST["occupation"];
+	$tel = $_POST["number"];
 
-	if (1==1) {
-		$query ="INSERT INTO user VALUES('', "
-			."'".$nom."',"
-			."'".$prenom."',"
-			."'".$email."',"
-			."'".$password."',"
-			."'".$oqp."',"
-			."'".$tel.")";
-echo $query;
+	if (1 == 1) {
+		$query = "INSERT INTO `freebed`.`user` VALUES (NULL, '1', '".$nom."', '".$prenom."', '".$email."', '".$password."', '".$oqp."', '".$tel."');";
 		$res = mysqli_query($connexion, $query) or die(mysqli_error($connexion));
 	}
 }
+	echo '<META HTTP-EQUIV="Refresh" Content="0; URL=retry.php">'
 ?>
