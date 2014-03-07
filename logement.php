c829cb2aa913ad25f15661afcb7a2deeca60c037
Wal Low <anti.factions@gmail.com>
2014-03-07 20:45:18 +0100

    Edit Header.php Logement.php

--- logement.php
+++ logement.php
@@ -1,3 +1,5 @@
+<?php include_once ("includes.php"); ?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -10,14 +12,12 @@
 		<title>FreeBed Location</title>
 	</head>
 
-	<body
-		<!-- Inclusion du header -->
+	<body>
 		<?php include_once ("header.php"); ?>
 		
-
 		<section>
 			<article id="logementname">
-				LOGEMENT
+				PHP QUI AFFICHE LE NOM DU LOGEMENT RICHARD ! WESH WESH
 			</article>
 		</section>
 
@@ -25,8 +25,31 @@
 			<article id="slider">
 				SLIDER
 			</article>
+		</section>
+		
+		<section>
 			<article id="logementinfos">
-				INFOS
+				<section id="logement_infos">
+					<article id="logement_text"> A partir de: </article>
+					<article id="logement_price"> 299€ </article>
+					<article id="logement_date">
+						<div class="logement_date">
+							Arrivée <br/> <input type="date" placeholder="jj/mm/aaaa" class="form-control form-logement_infos"/>
+						</div>
+						<div class="logement_date">
+							Départ <br/> <input type="date" placeholder="jj/mm/aaaa" class="form-control form-logement_infos"/>
+						</div>
+						<div class="logement_date">
+							Voyageurs <br/>
+							<select class="form-control form-logement_infos">
+								<option>1</option><option>2</option><option>4</option><option>5</option><option>6</option><option>7</option><option>8</option><option>9</option>
+							</select>
+						</div>
+					</article>
+				</section>
+				<section id="logementlouer">
+					<input type="submit" class="btn btn_louer" value="Réserver ce logement !"/>
+				</section>
 			</article>
 		</section>
 
@@ -34,11 +57,13 @@
 			<article id="logementdesc">
 				DESCRIPTION
 			</article>
+		</section>
+		
+		<section>
 			<article id="userinfos">
 				INFOS
 			</article>
 		</section>
-
 		<section>
 			<article id="allview">
 				AVIS
