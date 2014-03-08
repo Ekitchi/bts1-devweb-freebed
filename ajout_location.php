5ace54b0fa6e330451778d162368cd16bfad6686
Guilhem "Erw the Evangelist" MAS-PAITRAULT <guilhem.mas-paitrault@infosup.com>
2014-03-08 23:44:15 +0100

    Ajout du header et des divers includes nécessaires au fichier ajout_logement.php.

--- ajout_location.php
+++ ajout_location.php
@@ -1,16 +1,26 @@
+<!-- Traitements pré-HTML -->
+<?php
+	include_once ("includes.php");
+?>
 <!DOCTYPE html>
-
 <html>
 	<head>
-		<meta charset="utf-8">
-		<link rel="stylesheet" type="text"/>
-		<title>Nouveau Membre</title>
+		<title>FreeBed Location</title>
+		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+		<link rel="stylesheet" type="text/css" href="style.css"/>
+		<script src="jquery-1.10.2.min.js"></script>
+		<script type="text/javascript" src="scripts/prefixfree.min.js"></script>
+		<script type="text/javascript" src="scripts/jquery-2.1.0.js"></script>
+		<meta charset="UTF-8"/>
 	</head>
 	<body>
+		<!-- Inclusion du header -->
+		<?php include_once("header.php"); ?>
+		
 		<section id="enregistrement">
 			<div id="enr_encadrement">
 				<div id="nom">
-					<form method="POST" action="ajout.php">
+					<form method="POST" action="ajout_logement.php">
 						<fieldset>
 						<legend><h3><i>Formulaire d'enregistrement</i></h3></legend>
 						<p><input type="text" name="name" placeholder="Nom de votre logement" value="" required/></p>
