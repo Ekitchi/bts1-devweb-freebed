e0050341589a7fefc451cad496ac0191a5436fc7
Guilhem "Erw the Evangelist" MAS-PAITRAULT <guilhem.mas-paitrault@infosup.com>
2014-03-09 13:05:15 +0100

    Commit alakon pour tester le coup du password stocké dans le .netrc

--- recherche.php
+++ recherche.php
@@ -97,6 +97,7 @@
 		<section>
 			<article id="minimap" onclick="addMarqueurs();" onmousewheel="addMarqueurs();">
 			</article>
+			
 			<article id="filtre">
 				<fieldset style="padding:30px">
 				<legend><h4><i>Filtres de recherche</i></h4></legend>
