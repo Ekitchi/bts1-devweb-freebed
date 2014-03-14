7d45a5934c6cbfe1425651d589d6056276cfa0d8
Guilhem "Erw the Evangelist" MAS-PAITRAULT <guilhem.mas-paitrault@infosup.com>
2014-03-14 21:05:02 +0100

    Grosse aération du code et tout pleins de commentaires.

--- recherche.php
+++ recherche.php
@@ -2,7 +2,13 @@
 <?php
 	include_once ("includes.php");
 	include_once ("./classes/cl_Logement.php");
+	
 	$lieux = array();
+	
+	/*
+	 *  Attribue la recherche sur Paris par défaut si aucune ville ou Pays n'est précisé 
+	 *  grace au champs de recherche auto complété ou aux filtres
+	 */
 	if(isset($_POST["adresse"])) {
 		$adresse = $_POST["adresse"];	
 		if($adresse == "")
@@ -11,6 +17,10 @@
 	else
 		$adresse = $_POST["ville"].", ".$_POST["pays"];
 	
+	/*
+	 *  Construction de la requête prenant en charge tous les filtres dynamiquement
+	 */
+	// Construction d'un tableau de clauses WHERE
 	$where = new ArrayObject();
 	if (isset($_POST["filtre"])) {
 		if ($_POST["prix_min"] != "" && $_POST["prix_max"] != "") {
@@ -19,7 +29,6 @@
 		if ($_POST["surface"] != "") {
 			$where->append("surface <= ".$_POST["surface"]);
 		}
-		
 		if (isset($_POST["type"])) {
 			$type = $_POST["type"];
 			$in = "(0";
@@ -30,18 +39,19 @@
 			$where->append("type IN ".$in);
 		}
 		
+		// Construction de la chaine de WHERE clause AND clause, grâec au tableau $where[]
 		$clauses = "1";
 		foreach ($where as $e) {
 			$clauses = $clauses." AND ".$e;
 		}
-		echo $clauses;
-		
+		// La query magique générée grâce au super script de la mort \O/
 		$query = "SELECT id, latitude, longitude FROM bien WHERE ".$clauses.";";
 		$réponse = $bdd -> query($query);
 		while ($données = $réponse -> fetch()) {
 			$lieux[$données["id"]] = new Logement($données["id"], $données["latitude"], $données["longitude"]);
 		}
 	}
+	// Si aucun filtre n'est défini, gros SELECT de barbare sur la table bien
 	else {
 		$query = "SELECT id, latitude, longitude FROM bien";
 		$réponse = $bdd -> query($query);
@@ -63,58 +73,70 @@
 		<script type="text/javascript" src="scripts/jquery-2.1.0.js"></script>
 		<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
 		<script type="text/javascript" charset="UTF-8">
-						var geocoder;
-			var map;
-			var lieux = [{id: 0, lat: 0, lng: 0, nom: "0", img: "0", tarif: 0.00}<?php
-			foreach ($lieux as $lieu) { echo(",{id: " . $lieu -> getId() . ", lat: " . $lieu -> getLatitude() . ", lng: " . $lieu -> getLongitude() . ", nom: '" . $lieu -> getNom() . "', img: './data/photos/" . $lieu -> getPhoto() . "', tarif: " . $lieu -> getTarifJournee() . "}");
-			}
-		?>]
-			;
+			var geocoder;  //  Objet Google: Servira à obtenir une localisation GPS à partir d'une adresse
+			var map;       //  Objet Google: Sert à afficher la minimap
+			//  Tableau des différents lieux récupérés en php dans la requête pré-HTML
+			var lieux = [{id: 0, lat: 0, lng: 0, nom: "0", img: "0", tarif: 0.00}<?php foreach ($lieux as $lieu) { echo(",{id: " . $lieu -> getId() . ", lat: " . $lieu -> getLatitude() . ", lng: " . $lieu -> getLongitude() . ", nom: '" . $lieu -> getNom() . "', img: './data/photos/" . $lieu -> getPhoto() . "', tarif: " . $lieu -> getTarifJournee() . "}");}?>];
+			//  Tableau d'objets Google: Sert à stocker les marqueurs à afficher sur la minimap
 			var marqueurs = [];
 
+			/*
+			 *  Elements Javascripts préparés au chargement de la page
+			 */
 			function initialize() {
-			var post_adresse = "<?php echo($adresse); ?>"; // Récupération de l'adresse POSTée (j'aime mon humour de merde)
-				geocoder = new google.maps.Geocoder();
+				var post_adresse = "<?php echo($adresse); ?>";  // Récupération de l'adresse POSTée (j'aime mon humour de merde)
+				geocoder = new google.maps.Geocoder();  //  Initialisation de l'objet Geocode
 				var mapOptions = {center: new google.maps.LatLng(-34.397, 150.644), zoom: 12 };
 				map = new google.maps.Map(document.getElementById("minimap"), mapOptions);
 				codeAddress(post_adresse, geocoder);
 				}
 
-				function codeAddress(address) {
+			/*
+			 *  Fonction permettant de géocoder l'adresse passée par le champ de recherche header.
+			 */
+			function codeAddress(address) {
 				geocoder.geocode({ 'address': address}, function(results, status) {
-				if (status == google.maps.GeocoderStatus.OK) {
-				map.setCenter(results[0].geometry.location);
+					if (status == google.maps.GeocoderStatus.OK) {
+						// Définit le centre de la map aux coordonées renvoyées par l'objet geocoder
+						map.setCenter(results[0].geometry.location);
 
-				addMarqueurs();
-				}
-				else {
-				alert('Geocode was not successful for the following reason: ' + status);
-				}
+						addMarqueurs();
+						}
+					else {
+						//  Affiche une boite de dialogue alert en cas d'erreur
+						alert('Geocode was not successful for the following reason: ' + status);
+					}
 				});
-				}
+			}
 
-				function isInMap(lat, lng) {
+			/*
+			 *  Fonction permettant de déterminer si un point est dans le cadre de la map
+			 */
+			function isInMap(lat, lng) {
 				var map_bounds = map.getBounds();
 				var pos_marker = new google.maps.LatLng(lat, lng);
 
 				return map_bounds.contains(pos_marker);
-				}
+			}
 
-				function addMarqueurs() {
+			/*
+			 *   Fonction ajoutant sur la map tous les marqueurs inclus dans les limites de la map
+			 */
+			function addMarqueurs() {
 				cleanResults();
 				for (var i = 0; i < marqueurs.length; i++)
 				marqueurs[i].setMap(null);
 				marqueurs.splice(0);
 				for (var i = 0; i < lieux.length; i++) {
-				if (isInMap(lieux[i]["lat"], lieux[i]["lng"])) {
-				var pos_marqueurs = new google.maps.LatLng(lieux[i]["lat"], lieux[i]["lng"]);
-				marqueurs.push(new google.maps.Marker({map: map, position: pos_marqueurs, title: lieux[i]["nom"]}));
-				addSearchResults(i);
+					if (isInMap(lieux[i]["lat"], lieux[i]["lng"])) {
+						var pos_marqueurs = new google.maps.LatLng(lieux[i]["lat"], lieux[i]["lng"]);
+						marqueurs.push(new google.maps.Marker({map: map, position: pos_marqueurs, title: lieux[i]["nom"]}));
+						addSearchResults(i);
+					}
 				}
-				}
-				}
+			}
 
-				function addSearchResults (id) {
+			function addSearchResults (id) {
 				var zone_results = document.getElementById("allresults").firstChild.nextSibling.firstChild.nextSibling;
 				var resultat = document.createElement("li");
 				resultat.setAttribute("class", "Boxed");
@@ -134,7 +156,7 @@
 				zone_results.appendChild(resultat);
 				}
 
-				function cleanResults () {
+			function cleanResults () {
 				var zone_results = document.getElementById("allresults").firstChild.nextSibling.firstChild.nextSibling;
 				while (zone_results.firstChild)
 				zone_results.removeChild(zone_results.firstChild);
